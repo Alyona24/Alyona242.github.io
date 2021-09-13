4ada3cffaca7446d97d9dcb905d5de704753918d
Alyona24 <56617872+Alyona24@users.noreply.github.com>
2021-09-13 07:40:51 +0700

    Add files via upload

---
+++ test.php
@@ -0,0 +1,190 @@
+<html>
+	<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="style.css" type="text/css">
+<style type="text/css">
+   TABLE {
+   /* width: 300px; */
+    border-collapse: collapse; 
+   }
+   TD, TH {
+    padding: 3px;
+    border: 1px solid black;
+   }
+   TH {
+    background: #b0e0e6; 
+   }
+  </style>
+	
+<h1>Отдел кадров</h1>
+
+
+	</head>
+
+<form action="test.php" method="post">
+<button name="button1">Испытательный срок</button>
+<button name="button2">Уволенные</button>
+<button name="button3">Начальники</button>
+
+<br/>
+<br/>
+<body>
+<?php
+ $com = mysqli_connect("localhost","root","","test");
+//echo "Привет";
+//$sname="localhost";
+//$uname ="root";
+//$pass ="root";
+//$dbname ="test";
+//test1($sname,$uname,$pass,$dbname);
+
+
+//function test1($sname,$uname,$pass,$dbname)
+//{
+	 if( isset( $_POST['button1'] ) )
+ {
+
+	
+	
+$querysubmit1 = "SELECT * FROM `user` WHERE `created_at`> DATE_SUB(CURRENT_DATE(),INTERVAL 8 MONTH) ORDER BY `last_name` ASC ;";
+$result = mysqli_query($com,$querysubmit1 );
+$count = mysqli_num_rows($result);
+print "<table>";
+print "<tr>";
+print "<td>";
+print "<b>Фамилия</b>";
+print "</td>";
+print "<td>";
+print "<b>Имя</b>";
+print "</td>";
+print "<td>";
+print "<b>Отчество</b>";
+print "</td>";
+print "<td>";
+print "<b>Дата устройства</b>";
+print "</td>";
+print "</tr>";
+if ($count) {
+	while ($row = mysqli_fetch_array($result)){
+		print "<tr>";
+print "<td>";
+print_r($row['last_name']);
+print "</td>";
+print "<td>";
+print_r($row['first_name']);
+print "</td>";
+print "<td>";
+print_r($row['middle_name']);
+print "</td>";
+print "<td>";
+print_r($row['created_at']);
+print "</td>";
+print "</tr>";
+}
+print "</table>";}
+
+}
+
+
+	 if( isset( $_POST['button2'] ) )
+ {
+
+$querysubmit2 = "SELECT user.last_name,user.first_name, user.middle_name ,dismission_reason.description,user_dismission.is_active, user_dismission.created_at FROM `user_dismission` INNER JOIN dismission_reason ON `user_dismission`.`reason_id` = `dismission_reason`.`id` INNER JOIN user ON `user_dismission`.`user_id` = user.id WHERE `is_active` = 0 GROUP BY user_id ;";
+$result = mysqli_query($com,$querysubmit2 );
+$count = mysqli_num_rows($result);
+print "<table>";
+print "<tr>";
+print "<td>";
+print "<b>Фамилия</b>";
+print "</td>";
+print "<td>";
+print "<b>Имя</b>";
+print "</td>";
+print "<td>";
+print "<b>Отчество</b>";
+print "</td>";
+print "<td>";
+print "<b>Дата увольнения</b>";
+print "</td>";
+print "<td>";
+print "<b>Причина увольнения</b>";
+print "</td>";
+print "</tr>";
+if ($count) {
+	while ($row = mysqli_fetch_array($result)){
+		print "<tr>";
+print "<td>";
+print_r($row['last_name']);
+print "</td>";
+print "<td>";
+print_r($row['first_name']);
+print "</td>";
+print "<td>";
+print_r($row['middle_name']);
+print "</td>";
+print "<td>";
+print_r($row['description']);
+print "</td>";
+print "<td>";
+print_r($row['created_at']);
+print "</td>";
+print "</tr>";
+}
+print "</table>";}
+
+}
+
+
+
+	 if( isset( $_POST['button3'] ) )
+ {
+
+	
+$query2 = "INSERT INTO user (`id`, `first_name`, `last_name`, `middle_name`) VALUES (3,'vcfd','dfv','dffv' );";
+$query = "SELECT * FROM `user`";
+$querysubmit1 = "SELECT * FROM `user` WHERE `created_at`> DATE_SUB(CURRENT_DATE(),INTERVAL 8 MONTH) ORDER BY `last_name` ASC;";
+//$querydelete = "SELECT * FROM `user_dismission` WHERE `is_active` = 0;";
+//SELECT * FROM `user_dismission` INNER JOIN dismission_reason ON `user_dismission`.`reason_id` = `user_dismission`.`reason_id` WHERE `is_active` = 0;
+//$querysubmit2 = "SELECT user_dismission.user_id, user.last_name,user.first_name, user.middle_name,dismission_reason.name,user_dismission.is_active FROM `user_dismission` INNER JOIN dismission_reason ON `user_dismission`.`reason_id` = `dismission_reason`.`id` INNER JOIN user ON `user_dismission`.`user_id` = user.id WHERE `is_active` = 0 GROUP BY user_id ;";
+$querysubmit2 = "SELECT user.last_name,user.first_name, user.middle_name ,dismission_reason.description,user_dismission.is_active, user_dismission.created_at FROM `user_dismission` INNER JOIN dismission_reason ON `user_dismission`.`reason_id` = `dismission_reason`.`id` INNER JOIN user ON `user_dismission`.`user_id` = user.id WHERE `is_active` = 0 GROUP BY user_id ;";
+$querysubmit3 ="SELECT user_position.department_id, user_position.user_id, user.id, user.last_name, user.created_at FROM user_position INNER JOIN user ON user_position.user_id = user.id GROUP BY user_position.department_id  ORDER BY user_position.created_at DESC;";
+//SELECT department.id, department.name, department.leader_id,user_position.user_id FROM `department` INNER JOIN user_position on department.id = user_position.department_id GROUP BY leader_id ORDER BY user_position.created_ad DESC LIMIT 1;
+
+$querysubmit3 = "SELECT user_position.department_id, user_position.position_id, user.last_name FROM user_position INNER JOIN user ON user_position.user_id = user.id GROUP BY user_position.department_id  ORDER BY user_position.created_at DESC;";
+$result = mysqli_query($com,$querysubmit3 );
+$count = mysqli_num_rows($result);
+print "<table>";
+print "<tr>";
+print "<td>";
+print "<b>Отдел</b>";
+print "</td>";
+print "<td>";
+print "<b>Фамилия начальника</b>";
+print "</td>";
+print "<td>";
+print "<b>Фамилия сотрудника</b>";
+print "</td>";
+print "<td>";
+print "<b>Дата устройства</b>";
+print "</td>";
+print "</tr>";
+if ($count) {
+	while ($row = mysqli_fetch_array($result)){
+		print "<tr>";
+print "<td>";
+print_r($row['department_id']);
+print "</td>";
+print "<td>";
+print_r($row['last_name']);
+print "</td>";
+print "</tr>";
+}
+print "</table>";}
+
+}
+
+?>
+
+</form>
+</body>
+</html>
